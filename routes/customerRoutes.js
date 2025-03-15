7483762db0ae1a6619c3054b3615e5a10e03f350
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2025-03-15 16:56:16 +0530

    backend-avitrispices

---
+++ routes/customerRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const CustomerController = require("../Controllers/customerController");
+
+// User Routes
+router.post("/register", CustomerController.register);
+router.post("/login", CustomerController.login);
+router.post("/customers/loginWithMobileNumber", CustomerController.loginWithMobileNumber);
+router.get("/customers/:id", CustomerController.getProfile);
+router.get("/all", CustomerController.getAllUser);
+router.post("/customers/addAddress/:id", CustomerController.addAddress);
+router.put("/update/:id", CustomerController.updateProfile);
+
+router.delete("/delete/:id", CustomerController.deleteUser);
+
+module.exports = router;
